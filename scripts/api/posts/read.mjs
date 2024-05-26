66398cc18ed2e877e23e080a363d06acff377a89
gydalo <gylo_l@hotmail.com>
2024-05-26 18:24:36 +0200

    link twest

--- scripts/api/posts/read.mjs
+++ scripts/api/posts/read.mjs
@@ -1,5 +1,5 @@
-import { API_POST_BASE } from "./shared/constants.mjs";
-import { authFetch } from "./shared/authFetch.mjs";
+import { API_POST_BASE } from "/shared/constants.mjs";
+import { authFetch } from "/shared/authFetch.mjs";
 
 
 export async function getPosts() {
